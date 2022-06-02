2bb5b53e28b0c2d3d17d701df640818f40d12db7
Bennykillua <67695793+Bennykillua@users.noreply.github.com>
2022-06-02 16:23:20 +0100

    Update OOP.py

--- Object-oriented Programming in Python 🐍/OOP.py
+++ Object-oriented Programming in Python 🐍/OOP.py
@@ -1,4 +1,8 @@
 #defining a class
+
+[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/dataprofessor/beta-lactamase/blob/main/beta_lactamase_data.ipynb)
+
+#defining a class
 class Car:
     pass
 
